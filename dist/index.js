b8dbfae39c51c2cee9fa99245d8d00e21998cbd7
dmoon <dmoon.t@gmail.com>
2020-11-23 17:49:05 +0800

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -8,71 +8,101 @@ module.exports =
 const core = __webpack_require__(2559);
 const { LokaliseApi } = __webpack_require__(4150);
 
+// get inputs which defined in action metadata file
+const projectId = core.getInput('projectId');
+const apiKey = core.getInput('apiKey');
+const actionType = core.getInput('actionType');
+const actionPayload = JSON.parse(core.getInput('actionPayload'));
+
+console.log('projectId: ', projectId);
+console.log('actionType: ', actionType);
+console.log('123 actionPayload: ', actionPayload);
+
+const main = async () => {
+  // init API instance
+  const lokaliseApi = new LokaliseApi({ apiKey: apiKey });
+
+  const create = async (branchName) => {
+    return await lokaliseApi.branches.create({ name: branchName }, { project_id: projectId });
+  };
+
+  const findByName = async (branchName) => {
+    const branchList = await lokaliseApi.branches.list({ project_id: projectId });
+    const foundBranch = branchList.find((element) => element.name === branchName);
+    console.log(JSON.stringify(foundBranch));
+    return foundBranch;
+  };
+
+  const merge = async ({
+    branchNameToMerge,
+    targeBranchName,
+    throwBranchNotExistError = false,
+  }) => {
+    console.log('branchNameToMerge: ', branchNameToMerge);
+    console.log('targeBranchName: ', targeBranchName);
+    const headBranch = await findByName(branchNameToMerge);
+    console.log('headBranch: ', headBranch);
+    const baseBranch = await findByName(targeBranchName);
+    console.log('baseBranch: ', baseBranch);
+
+    if (throwBranchNotExistError) {
+      if (!headBranch) {
+        throw new Error(`Merge failed: The head branch ${branchNameToMerge} is not exist.`);
+      }
+
+      if (!baseBranch) {
+        throw new Error(`Merge failed: The base branch ${targeBranchName} is not exist.`);
+      }
+    }
+
+    return await lokaliseApi.branches.merge(
+      headBranch.branch_id,
+      { project_id: projectId },
+      { target_branch_id: baseBranch.branch_id }
+    );
+  };
+
+  const createAndBackport = async ({ branchNameToCreate, branchNameToBackport, throwBranchNotExistError }) => {
+    await create(branchNameToCreate);
+
+    return await merge({
+      branchNameToMerge: branchNameToBackport,
+      targeBranchName: branchNameToCreate,
+      throwBranchNotExistError,
+    });
+  };
+
+  switch (actionType) {
+    case 'findByName':
+      const foundBranch = await findByName(actionPayload);
+      core.setOutput('findByNameResult', foundBranch);
+      break;
+
+    case 'create':
+      const createdResult = await create(actionPayload);
+      core.setOutput('createResult', createdResult);
+      break;
+
+    case 'merge':
+      const mergedResult = await merge(actionPayload);
+      core.setOutput('mergeResult', mergedResult);
+      break;
+
+    case 'createAndBackport':
+      const createAndBackportResult = await createAndBackport(actionPayload);
+      core.setOutput('createAndBackportResult', createAndBackportResult);
+      break;
+  }
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`)
+};
+
 try {
-    // get inputs which defined in action metadata file
-    const projectId = core.getInput('projectId');
-    console.log('projectId: ', projectId);
-    const apiKey = core.getInput('apiKey');
-    const actionType = core.getInput('actionType');
-    console.log('actionType: ', actionType);
-    const actionPayload = core.getInput('actionPayload');
-    console.log('actionPayload: ', actionPayload);
-
-    // init API instance
-    const lokaliseApi = new LokaliseApi({ apiKey: apiKey });
-
-    const create = async (branchName) =>
-        await lokaliseApi.branches.create({ name: branchName }, { project_id: projectId });
-
-    const merge = async ({ branchIdToMerge, targetBranchId }) =>
-        await lokaliseApi.branches.merge(
-            branchIdToMerge,
-            { project_id: projectId },
-            { target_branch_id: targetBranchId }
-        );
-
-    const findByName = async (branchName) => {
-        const branchList = await lokaliseApi.branches.list({ project_id: projectId });
-        const foundBranch = branchList.find((element) => element.name === branchName);
-        console.log(JSON.stringify(foundBranch));
-        return foundBranch;
-    };
-
-    (async () => {
-        switch (actionType) {
-            case 'findByName':
-                const foundBranch = await findByName(actionPayload);
-                core.setOutput('findByNameResult', foundBranch);
-                break;
-
-            case 'create':
-                const createdResult = await create(actionPayload);
-                core.setOutput('createResult', createdResult);
-                break;
-
-            case 'merge':
-                const mergedResult = await merge(actionPayload);
-                core.setOutput('mergeResult', mergedResult);
-                break;
-
-            case 'createAndBackport':
-                const { branchNameToCreate, branchNameToBackport } = actionPayload;
-                const branchToBackport = await findByName(branchNameToBackport);
-                const branchToCreate = await create(branchNameToCreate);
-                const createAndBackportResult = merge({
-                    branchIdToMerge: branchToBackport.branch_id,
-                    targetBranchId: branchToCreate.branch_id,
-                });
-                core.setOutput('createAndBackportResult', createAndBackportResult);
-                break;
-        }
-    })();
-
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`)
+  main();
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
 
 
