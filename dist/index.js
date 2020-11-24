0e5a31c40f3d395950b0bb81dd4f4c45d847e042
DMoon <dmoon.t@gmail.com>
2020-11-24 16:20:17 +0800

    Update coding style

--- dist/index.js
+++ dist/index.js
@@ -20,102 +20,102 @@ console.log('actionPayload: ', actionPayload);
 
 const formatBranchName = (name) => name.replace(/\./g, '_');
 
+// init API instance
+const lokaliseApi = new LokaliseApi({ apiKey: apiKey });
+
+const create = async (branchName) => {
+  const formattedBranchName = formatBranchName(branchName);
+  return await lokaliseApi.branches.create(
+    { name: formattedBranchName },
+    { project_id: projectId }
+  );
+};
+
+const findByName = async (branchName) => {
+  try {
+    const formattedBranchName = formatBranchName(branchName);
+    const branchList = await lokaliseApi.branches.list({ project_id: projectId });
+    const foundBranch = branchList.find((element) => element.name === formattedBranchName);
+    return foundBranch;
+  } catch(error) {
+    throw error;
+  }
+};
+
+const merge = async ({ branchNameToMerge, targeBranchName, throwBranchNotExistError = false }) => {
+  let headBranch, baseBranch;
+  try {
+    headBranch = await findByName(formatBranchName(branchNameToMerge));
+    console.log('headBranch: ', headBranch);
+    baseBranch = await findByName(formatBranchName(targeBranchName));
+    console.log('baseBranch: ', baseBranch);
+
+    if (!headBranch || !baseBranch) {
+      if (!headBranch) {
+        const errorMessage = `Merge failed: The head branch ${branchNameToMerge} is not exist.`;
+
+        console.log(errorMessage);
+        if (throwBranchNotExistError) {
+          throw new Error(errorMessage);
+        }
+      }
+
+      if (!baseBranch) {
+        const errorMessage = `Merge failed: The head branch ${branchNameToMerge} is not exist.`;
+
+        console.log(errorMessage);
+        if (throwBranchNotExistError) {
+          throw new Error(errorMessage);
+        }
+      }
+
+      return null;
+    }
+    return await lokaliseApi.branches.merge(
+      headBranch.branch_id,
+      { project_id: projectId },
+      { target_branch_id: baseBranch.branch_id }
+    );
+  } catch (error) {
+    const pullRequestUrl = `https://app.lokalise.com/merge/${projectId}/${headBranch.branch_id}/${baseBranch.branch_id}`;
+
+    console.log('Merge error: ', error);
+    console.log('Pull request url: ', pullRequestUrl);
+
+    core.setOutput(
+      'error',
+      JSON.stringify({
+        error,
+        data: {
+          pullRequestUrl,
+        },
+      })
+    );
+
+    throw error;
+  }
+};
+
+const createAndBackport = async ({
+  branchNameToCreate,
+  branchNameToBackport,
+  throwBranchNotExistError,
+}) => {
+  try {
+    await create(branchNameToCreate);
+
+    return await merge({
+      branchNameToMerge: branchNameToBackport,
+      targeBranchName: branchNameToCreate,
+      throwBranchNotExistError,
+    });
+  } catch(error) {
+    throw error;
+  }
+};
+
 const main = async () => {
   try {
-    // init API instance
-    const lokaliseApi = new LokaliseApi({ apiKey: apiKey });
-
-    const create = async (branchName) => {
-      const formattedBranchName = formatBranchName(branchName);
-      return await lokaliseApi.branches.create(
-        { name: formattedBranchName },
-        { project_id: projectId }
-      );
-    };
-
-    const findByName = async (branchName) => {
-      const formattedBranchName = formatBranchName(branchName);
-      const branchList = await lokaliseApi.branches.list({ project_id: projectId });
-      const foundBranch = branchList.find((element) => element.name === formattedBranchName);
-      return foundBranch;
-    };
-
-    const merge = async ({
-      branchNameToMerge,
-      targeBranchName,
-      throwBranchNotExistError = false,
-    }) => {
-      let headBranch, baseBranch;
-      try {
-        headBranch = await findByName(formatBranchName(branchNameToMerge));
-        console.log('headBranch: ', headBranch);
-        baseBranch = await findByName(formatBranchName(targeBranchName));
-        console.log('baseBranch: ', baseBranch);
-
-        if (!headBranch || !baseBranch) {
-          if (!headBranch) {
-            const errorMessage = `Merge failed: The head branch ${branchNameToMerge} is not exist.`;
-
-            console.log(errorMessage);
-            if (throwBranchNotExistError) {
-              throw new Error(errorMessage);
-            }
-          }
-
-          if (!baseBranch) {
-            const errorMessage = `Merge failed: The head branch ${branchNameToMerge} is not exist.`;
-
-            console.log(errorMessage);
-            if (throwBranchNotExistError) {
-              throw new Error(errorMessage);
-            }
-          }
-
-          return null;
-        }
-        console.log('before merge');
-        const result = await lokaliseApi.branches.merge(
-          headBranch.branch_id,
-          { project_id: projectId },
-          { target_branch_id: baseBranch.branch_id }
-        );
-        console.log('result: ', result);
-
-        return result;
-      } catch (error) {
-        const pullRequestUrl = `https://app.lokalise.com/merge/${projectId}/${headBranch.branch_id}/${baseBranch.branch_id}`;
-
-        console.log('Merge error: ', error);
-        console.log('Pull request url: ', pullRequestUrl);
-
-        core.setOutput(
-          'error',
-          JSON.stringify({
-            error,
-            data: {
-              pullRequestUrl,
-            },
-          })
-        );
-
-        throw error;
-      }
-    };
-
-    const createAndBackport = async ({
-      branchNameToCreate,
-      branchNameToBackport,
-      throwBranchNotExistError,
-    }) => {
-      await create(branchNameToCreate);
-
-      return await merge({
-        branchNameToMerge: branchNameToBackport,
-        targeBranchName: branchNameToCreate,
-        throwBranchNotExistError,
-      });
-    };
-
     let result;
 
     switch (actionType) {
@@ -152,6 +152,7 @@ const main = async () => {
 };
 
 main();
+
 
 /***/ }),
 
