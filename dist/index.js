117fecfb22669d6fb71f13c9c6bef17e5625ad11
DMoon <dmoon.t@gmail.com>
2020-11-24 12:53:48 +0800

    Catch error

--- dist/index.js
+++ dist/index.js
@@ -21,114 +21,118 @@ console.log('actionPayload: ', actionPayload);
 const formatBranchName = (name) => name.replace(/\./g, '_');
 
 const main = async () => {
-  // init API instance
-  const lokaliseApi = new LokaliseApi({ apiKey: apiKey });
+  try {
+    // init API instance
+    const lokaliseApi = new LokaliseApi({ apiKey: apiKey });
 
-  const create = async (branchName) => {
-    const formattedBranchName = formatBranchName(branchName);
-    return await lokaliseApi.branches.create(
-      { name: formattedBranchName },
-      { project_id: projectId }
-    );
-  };
-
-  const findByName = async (branchName) => {
-    const formattedBranchName = formatBranchName(branchName);
-    const branchList = await lokaliseApi.branches.list({ project_id: projectId });
-    const foundBranch = branchList.find((element) => element.name === formattedBranchName);
-    return foundBranch;
-  };
-
-  const merge = async ({
-    branchNameToMerge,
-    targeBranchName,
-    throwBranchNotExistError = false,
-  }) => {
-    try {
-      const headBranch = await findByName(formatBranchName(branchNameToMerge));
-      console.log('headBranch: ', headBranch);
-      const baseBranch = await findByName(formatBranchName(targeBranchName));
-      console.log('baseBranch: ', baseBranch);
-
-      if (!headBranch || !baseBranch) {
-        if (!headBranch) {
-          const errorMessage = `Merge failed: The head branch ${branchNameToMerge} is not exist.`;
-
-          console.log(errorMessage);
-          if (throwBranchNotExistError) {
-            throw new Error(errorMessage);
-          }
-        }
-
-        if (!baseBranch) {
-          const errorMessage = `Merge failed: The head branch ${branchNameToMerge} is not exist.`;
-
-          console.log(errorMessage);
-          if (throwBranchNotExistError) {
-            throw new Error(errorMessage);
-          }
-        }
-
-        return null;
-      }
-      console.log('before merge');
-      const result = await lokaliseApi.branches.merge(
-        headBranch.branch_id,
-        { project_id: projectId },
-        { target_branch_id: baseBranch.branch_id }
+    const create = async (branchName) => {
+      const formattedBranchName = formatBranchName(branchName);
+      return await lokaliseApi.branches.create(
+        { name: formattedBranchName },
+        { project_id: projectId }
       );
-      console.log('result: ', result);
+    };
 
-      return result;
-    } catch (error) {
-      console.log('merge error: ', error);
-      throw error;
-    }
-  };
+    const findByName = async (branchName) => {
+      const formattedBranchName = formatBranchName(branchName);
+      const branchList = await lokaliseApi.branches.list({ project_id: projectId });
+      const foundBranch = branchList.find((element) => element.name === formattedBranchName);
+      return foundBranch;
+    };
 
-  const createAndBackport = async ({
-    branchNameToCreate,
-    branchNameToBackport,
-    throwBranchNotExistError,
-  }) => {
-    await create(branchNameToCreate);
+    const merge = async ({
+      branchNameToMerge,
+      targeBranchName,
+      throwBranchNotExistError = false,
+    }) => {
+      try {
+        const headBranch = await findByName(formatBranchName(branchNameToMerge));
+        console.log('headBranch: ', headBranch);
+        const baseBranch = await findByName(formatBranchName(targeBranchName));
+        console.log('baseBranch: ', baseBranch);
 
-    return await merge({
-      branchNameToMerge: branchNameToBackport,
-      targeBranchName: branchNameToCreate,
+        if (!headBranch || !baseBranch) {
+          if (!headBranch) {
+            const errorMessage = `Merge failed: The head branch ${branchNameToMerge} is not exist.`;
+
+            console.log(errorMessage);
+            if (throwBranchNotExistError) {
+              throw new Error(errorMessage);
+            }
+          }
+
+          if (!baseBranch) {
+            const errorMessage = `Merge failed: The head branch ${branchNameToMerge} is not exist.`;
+
+            console.log(errorMessage);
+            if (throwBranchNotExistError) {
+              throw new Error(errorMessage);
+            }
+          }
+
+          return null;
+        }
+        console.log('before merge');
+        const result = await lokaliseApi.branches.merge(
+          headBranch.branch_id,
+          { project_id: projectId },
+          { target_branch_id: baseBranch.branch_id }
+        );
+        console.log('result: ', result);
+
+        return result;
+      } catch (error) {
+        console.log('merge error: ', error);
+        throw error;
+      }
+    };
+
+    const createAndBackport = async ({
+      branchNameToCreate,
+      branchNameToBackport,
       throwBranchNotExistError,
-    });
-  };
+    }) => {
+      await create(branchNameToCreate);
 
-  let result;
+      return await merge({
+        branchNameToMerge: branchNameToBackport,
+        targeBranchName: branchNameToCreate,
+        throwBranchNotExistError,
+      });
+    };
 
-  switch (actionType) {
-    case 'findByName':
-      const foundBranchResult = await findByName(actionPayload);
-      result = foundBranchResult;
-      break;
+    let result;
 
-    case 'create':
-      const createdResult = await create(actionPayload);
-      result = createdResult;
-      break;
+    switch (actionType) {
+      case 'findByName':
+        const foundBranchResult = await findByName(actionPayload);
+        result = foundBranchResult;
+        break;
 
-    case 'merge':
-      const mergedResult = await merge(actionPayload);
-      result = mergedResult;
-      break;
+      case 'create':
+        const createdResult = await create(actionPayload);
+        result = createdResult;
+        break;
 
-    case 'createAndBackport':
-      const createAndBackportResult = await createAndBackport(actionPayload);
-      result = createAndBackportResult;
-      break;
+      case 'merge':
+        const mergedResult = await merge(actionPayload);
+        result = mergedResult;
+        break;
+
+      case 'createAndBackport':
+        const createAndBackportResult = await createAndBackport(actionPayload);
+        result = createAndBackportResult;
+        break;
+    }
+
+    console.log('result: ', result);
+    core.setOutput('result', JSON.stringify(result));
+    // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`)
+  } catch (error) {
+    throw error;
   }
-
-  console.log('result: ', result);
-  core.setOutput('result', JSON.stringify(result));
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`)
 };
 
 try {
