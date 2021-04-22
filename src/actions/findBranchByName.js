27a3e1fd8709db54cbd0c449e819d885f00a8c33
陳乙山 <pipio1994@gmail.com>
2021-04-22 17:09:38 +0800

    feat: fetch branch list with max limit

--- src/actions/findBranchByName.js
+++ src/actions/findBranchByName.js
@@ -3,11 +3,14 @@ import lokaliseApi from "../lokaliseApi";
 import formatBranchName from "../utils/formatBranchName";
 const projectId = core.getInput("projectId");
 
+const BRANCH_LISTING_MAX_LIMIT = 5000;
+
 const findBranchByName = async (branchName) => {
   try {
     const formattedBranchName = formatBranchName(branchName);
     const branchList = await lokaliseApi.branches.list({
       project_id: projectId,
+      limit: BRANCH_LISTING_MAX_LIMIT,
     });
     const foundBranch = branchList.find(
       (element) => element.name === formattedBranchName
